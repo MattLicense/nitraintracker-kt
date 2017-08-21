f28bbdbedbe46a416a395f22fccf6f56df051a89
Matt License <matt.license@neueda.com>
2017-08-21 17:16:28 +0100

    Removing search button from view when the stations selected are the same.

--- app/src/main/java/com/licensem/nitraintracker/listeners/StationSelectorListener.kt
+++ app/src/main/java/com/licensem/nitraintracker/listeners/StationSelectorListener.kt
@@ -17,27 +17,28 @@ class StationSelectorListener: AdapterView.OnItemSelectedListener, AnkoLogger {
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         val parentView = parent?.parent as View
 
-        var resultsView: ListView = parentView.findViewById<ListView>(R.id.result_view)
+        var resultsView = parentView.findViewById<ListView>(R.id.result_view)
 
-        val originSpinner = parentView?.findViewById<Spinner>(R.id.originSelector)
-        val destinationSpinner = parentView?.findViewById<Spinner>(R.id.destinationSelector)
-
-        val origin = originSpinner.selectedItem.toString()
-        val dest = destinationSpinner.selectedItem.toString()
+        val origin = parentView.findViewById<Spinner>(R.id.originSelector).selectedItem.toString()
+        val dest = parentView.findViewById<Spinner>(R.id.destinationSelector).selectedItem.toString()
 
         if(resultsView.tag != origin + "-" + dest) {
             info { "Clearing results after a station was changed" }
             resultsView.adapter = ArrayAdapter<Service>(parentView.context, android.R.layout.list_content, emptyArray())
         }
 
-        if (origin != "" && dest != "" && origin != dest) {
-            parentView?.findViewById<Button>(R.id.search_button).apply { this.isEnabled = true }
+        // only enable the search button if origin / destination have been selected and they're different stations
+        var searchButton = parentView.findViewById<Button>(R.id.search_button)
+        if(origin != "" && dest != "" && origin != dest) {
+            searchButton.isEnabled = true
+            searchButton.visibility = View.VISIBLE
+        } else {
+            searchButton.isEnabled = false
+            searchButton.visibility = View.INVISIBLE
         }
 
         // make sure the favourites button is highlighted if this is a favourite
-        parentView?.findViewById<ToggleButton>(R.id.favourite_button).apply {
-            this.isChecked = FavouritesDatabase.exists(parent?.context, FavouriteTrip(origin,dest))
-        }
+        parentView.findViewById<ToggleButton>(R.id.favourite_button).isChecked = FavouritesDatabase.exists(parent?.context, FavouriteTrip(origin,dest))
     }
 
 }
